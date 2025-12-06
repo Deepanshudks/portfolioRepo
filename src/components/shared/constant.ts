137f2f11978618d96595a9009352039fa68585c2
Kuldeep Gaud <140884017+Deepanshudks@users.noreply.github.com>
2025-12-06 20:16:31 +0530

    resume updated

--- src/components/shared/constant.ts
+++ src/components/shared/constant.ts
@@ -130,7 +130,7 @@ export const skillsData = [
 ];
 
 export const resumeLink =
-  "https://drive.google.com/file/d/1PWn6G53SLiEklKlAXu63uQJJfvXSJyp-/view?usp=drive_link";
+  "https://drive.google.com/file/d/1FtONOvHoQkPoMZplE4MgIjfdVIUKaA6e/view?usp=drive_link";
 
 export const socialLinks = [
   { href: "https://x.com/Deepanshudk08", icon: BsTwitter, label: "Twitter" },
--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -3,10 +3,8 @@
  * These run only on the server and handle external API calls
  */
 
-export { default as serverAxios } from './axios';
-export { githubServerService } from './github.service';
-export { spotifyServerService } from './spotify.service';
+export { default as serverAxios } from "./axios";
+export { spotifyServerService } from "./spotify.service";
 
 // Re-export types
-export type { GitHubData, GitHubUser, GitHubRepo, GitHubEvent } from './github.service';
-export type { NowPlayingData } from './spotify.service';
+export type { NowPlayingData } from "./spotify.service";
